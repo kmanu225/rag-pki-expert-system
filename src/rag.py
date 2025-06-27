41ac303306458bd1cdd00b7049e2a599516dc799
macxpython <macxpython@gmail.com>
2025-06-27 20:40:28 +0200

    use smaller model

--- src/rag.py
+++ src/rag.py
@@ -2,7 +2,7 @@ from llm_loader import load_llm
 from vector_loader import load_and_split_texts, store_passages_in_chromadb
 
 
-model = "google/gemma-2-2b-it"
+model = "HuggingFaceTB/SmolLM2-1.7B-Instruct"
 llm_pipeline = load_llm(model_name=model)
 
 knowledge_folder = "src/knowledge"
