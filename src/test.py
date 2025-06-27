a5e253d08e08c78afb365ba1e2da2be187304312
macxpython <macxpython@gmail.com>
2025-06-27 20:40:07 +0200

    update test

--- src/test.py
+++ src/test.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
 
     hf_token = "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"
     model = "google/gemma-2-2b-it"
-    llm_pipeline = load_llm(model_name=model, hf_token=hf_token)
+    llm_pipeline = load_llm(model_name=model)
 
     # Example prompt
     response = llm_pipeline("What is the capital of France?", max_new_tokens=20)
     print(response)
 
-    knowledge_folder = "knowledge"
+    knowledge_folder = "src/knowledge"
     passages = load_and_split_texts(knowledge_folder)
     collection = store_passages_in_chromadb(passages)
     print(f"Loaded {len(passages)} passages.")
